a95c5a489f39af1b143227e111920c0f06ecd9e0
Tatjana-M <mordvynova@gmail.com>
2023-11-22 13:52:00 +0100

    next lessons

--- script.js
+++ script.js
@@ -63,25 +63,25 @@
 // const usdCurr = 28;
 // const eurCurr = 32;
 
-// function convert(amount,curr){
-// console.log(curr * amount);
-// }
+function convert(amount, curr) {
+    console.log(curr * amount);
+}
 
-// convert (500,usdCurr);
-// convert (500, eurCurr);
+convert(500, usdCurr);
+convert(500, eurCurr);
 
-// const usdCurr = 28;
-// const discount = 0.9;
+const usdCurr = 28;
+const discount = 0.9;
 
-// function convert(amount,curr){
-// return curr * amount;
-// }
+function convert(amount, curr) {
+    return curr * amount;
+}
 
-// function promotion(result) {
-//     console.log(result * discount);
-// }
+function promotion(result) {
+    console.log(result * discount);
+}
 
-// promotion (convert (500,usdCurr));
+promotion(convert(500, usdCurr));
 
 // function test() {
 //     for (let i=0; i<5; i++){
@@ -203,3 +203,77 @@ function writeYourGenres() {
     }
 }
 writeYourGenres();
+
+function calculateVolumeAndArea(edgeLength) {
+    if (Number.isInteger(edgeLength) && Math.sign(edgeLength) > 0) {
+        let area, volume = 0;
+
+        area = (edgeLength * edgeLength * 6);
+
+        volume = (edgeLength * edgeLength * edgeLength);
+
+        return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
+    } else {
+        return 'При вычислении произошла ошибка';
+    }
+}
+console.log(calculateVolumeAndArea(5));
+
+
+
+function getCoupeNumber(seatNumber) {
+    if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+
+    if (seatNumber === 0 || seatNumber > 36) {
+        return "Таких мест в вагоне не существует";
+    }
+
+    return Math.ceil(seatNumber / 4);
+    // тут очень много вариантов решения, но выбрал один из элегантных :)
+}
+
+console.log(getCoupeNumber(33));
+
+// function getTimeFromMinutes(minutesTotal) {
+//     if (typeof (minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+//         return "Ошибка, проверьте данные";
+//     }
+
+//     const hours = Math.floor(minutesTotal / 60);
+//     const minutes = minutesTotal % 60;
+
+//     let hoursStr = '';
+
+//     switch (hours) {
+//         case 0:
+//             hoursStr = 'часов';
+//             break;
+//         case 1:
+//             hoursStr = 'час';
+//             break;
+//         case 2:
+//         case 3:
+//         case 4:
+//             hoursStr = 'часа';
+//             break;
+//         default:
+//             hoursStr = 'часов';
+//     }
+
+//     return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+// }
+
+// getTimeFromMinutes(180)
+
+function leanJS(lang, callback) {
+    console.log(`I learn ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('I completed this task');
+}
+
+leanJS('JS', done);
